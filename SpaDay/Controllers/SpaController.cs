198066d969951634881a896cf8625c5bf3dc781f
Monica C <tigina18@gmail.com>
2020-11-10 19:34:07 -0600

    Finished studio, no bonus

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -42,6 +42,10 @@ namespace SpaDay.Controllers
         [Route("/spa")]
         public IActionResult Menu(string name, string skintype, string manipedi)
         {
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;
+            ViewBag.manipedi = manipedi;
+
             List<string> facials = new List<string>()
             {
                 "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
@@ -55,6 +59,9 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+
+            ViewBag.appropriateFacials = appropriateFacials;
+
             return View();
         }
 
